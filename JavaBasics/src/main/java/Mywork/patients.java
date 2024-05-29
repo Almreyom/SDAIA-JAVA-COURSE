b9af37ef60a7f38d1e11efba67989e452ce6de4f
Reyom <Reyomalmalki@gmail.com>
2024-05-29 12:08:19 +0300

    pat class

---
+++ JavaBasics/src/main/java/Mywork/patients.java
@@ -0,0 +1,14 @@
+package Mywork;
+
+public class patients {
+    String name;
+    double wieght;
+    double height;
+
+    public patients ( String n , double w , double h ){
+        name = n ;
+        wieght = w ;
+        height = h;
+    }
+
+}
