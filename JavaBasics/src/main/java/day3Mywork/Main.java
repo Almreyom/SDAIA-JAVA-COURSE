36832ba52927909aa6b395f0ad4c7349c2bd6e93
Reyom <Reyomalmalki@gmail.com>
2024-05-30 10:47:10 +0300

    extends

---
+++ JavaBasics/src/main/java/day3Mywork/Main.java
@@ -0,0 +1,17 @@
+package day3Mywork;
+
+public class Main {
+    public  static void main(String []args){
+
+        Account x = new Account("Reyom",50000,5.5);
+
+        System.out.println(x.name);
+        System.out.println(x.getBalance());
+        System.out.println(x.annanualRate);
+
+SavingAccount x1 = new SavingAccount(100,"Dhay", 500 , 1.1);
+    CheckingAccount x2 = new CheckingAccount("masgael",400, 2.2);
+
+
+
+}}
